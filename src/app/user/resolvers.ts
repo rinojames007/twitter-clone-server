b5708786d0043b5c9e21431efd7336480b8c1d17
Rino <rinojames.krl@gmail.com>
2023-10-31 19:26:16 +0530

    complete authentication flow

--- src/app/user/resolvers.ts
+++ src/app/user/resolvers.ts
@@ -58,9 +58,11 @@ const queries = {
 
     },
     getCurrentUser: async(parent: any, args: any, ctx: GraphqlContext) => {
-        console.log(ctx);
         const id = ctx.user?.id;
         if(!id) return null;
+
+        const user = await prismaClient.user.findUnique({ where: { id }});
+        return user;
 
     }
 };
