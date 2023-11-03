a12143b3b42cb0c9c9143f2958ce8a751678477a
Rino <rinojames.krl@gmail.com>
2023-11-03 20:45:59 +0530

    add tweet feed and create feed

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,22 +1,22 @@
-import express from 'express';
+import express from "express";
 import bodyParser from "body-parser";
-import cors from 'cors';
+import cors from "cors";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 
-import { User } from './user'
-import { Tweet  } from './tweet'
-import {GraphqlContext} from "../interfaces";
+import { User } from "./user";
+import { Tweet } from "./tweet";
+import { GraphqlContext } from "../interfaces";
 import JWTService from "../services/jwt";
 
 export async function initServer() {
-    const app = express();
+  const app = express();
 
-    app.use(bodyParser.json())
-    app.use(cors())
+  app.use(bodyParser.json());
+  app.use(cors());
 
-    const graphqlServer = new ApolloServer<GraphqlContext>({
-        typeDefs: `
+  const graphqlServer = new ApolloServer<GraphqlContext>({
+    typeDefs: `
             ${User.types}
             ${Tweet.types}
             
@@ -30,30 +30,35 @@ export async function initServer() {
             }
             
         `,
-        resolvers: {
-            Query: {
-                ...User.resolvers.queries,
-                ...Tweet.resolvers.queries
-            },
-            Mutation: {
-                ...Tweet.resolvers.mutations
-            },
-            ...Tweet.resolvers.extraResolvers,
-            ...User.resolvers.extraResolvers,
-        }
-    });
+    resolvers: {
+      Query: {
+        ...User.resolvers.queries,
+        ...Tweet.resolvers.queries,
+      },
+      Mutation: {
+        ...Tweet.resolvers.mutations,
+      },
+      ...Tweet.resolvers.extraResolvers,
+      ...User.resolvers.extraResolvers,
+    },
+  });
 
-    await graphqlServer.start();
+  await graphqlServer.start();
 
-    app.use('/graphql', expressMiddleware(graphqlServer, {
-        context: async({req, res}) => {
-            return {
-                user: req.headers.authorization
-                    ? JWTService.decodeToken(req.headers.authorization.split('Bearer ')[1])
-                    : undefined
-            }
-        }
-    }));
+  app.use(
+    "/graphql",
+    expressMiddleware(graphqlServer, {
+      context: async ({ req, res }) => {
+        return {
+          user: req.headers.authorization
+            ? JWTService.decodeToken(
+                req.headers.authorization.split("Bearer ")[1]
+              )
+            : undefined,
+        };
+      },
+    })
+  );
 
-    return app;
+  return app;
 }
