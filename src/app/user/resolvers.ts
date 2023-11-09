ffc1f3da8ecab84fe1fbdd85d557218d7b5a3e42
Rino <rinojames.krl@gmail.com>
2023-11-09 08:38:03 +0530

    complete follow unfollow logic

--- src/app/user/resolvers.ts
+++ src/app/user/resolvers.ts
@@ -4,7 +4,6 @@ import JWTService from "../../services/jwt";
 import { GraphqlContext } from "../../interfaces";
 import { User } from "@prisma/client";
 import UserService from "../../services/user";
-import { User } from ".";
 
 const queries = {
   verifyGoogleToken: async (parent: any, { token }: { token: string }) => {
@@ -29,13 +28,44 @@ const extraResolvers = {
   User: {
     tweets: (parent: User) =>
       prismaClient.tweet.findMany({ where: { author: { id: parent.id } } }),
+    followers: async (parent: User) => {
+      const result = await prismaClient.follows.findMany({
+        where: { following: { id: parent.id } },
+        include: {
+          follower: true,
+        },
+      });
+      return result.map((el) => el.follower);
+    },
+    following: async (parent: User) => {
+      const result = await prismaClient.follows.findMany({
+        where: { follower: { id: parent.id } },
+        include: {
+          following: true,
+        },
+      });
+      return result.map((el) => el.following);
+    },
   },
 };
 
 const mutations = {
-  followUser: async (parent: any, {to}: { to: string }, ctx: GraphqlContext) => {
+  followUser: async (
+    parent: any,
+    { to }: { to: string },
+    ctx: GraphqlContext
+  ) => {
     if (!ctx.user || !ctx.user.id) throw new Error("Unauthenticated");
     await UserService.followUser(ctx.user.id, to);
+    return true;
+  },
+  unfollowUser: async (
+    parent: any,
+    { to }: { to: string },
+    ctx: GraphqlContext
+  ) => {
+    if (!ctx.user || !ctx.user.id) throw new Error("Unauthenticated");
+    await UserService.unfollowUser(ctx.user.id, to);
     return true;
   },
 };
